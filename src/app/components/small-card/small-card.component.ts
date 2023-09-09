044668563ba8f4ad6317dc50dec470551f7d7db4
BiteTheBullet47 <121462847+Matt-Gary@users.noreply.github.com>
2023-09-09 12:12:16 +0100

    New blog project

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardText: string = ""
+  @Input()
+  Id:string = ""
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,13 @@
+export const dataFake = [{
+  "id": "1",
+  "title": "Lorem Ipsum",
+  "description": "Hello World",
+  "photoCover": "https://imgs.search.brave.com/eRrYyxK44yuM6rdGTV1CgOxchpCjJvR_tpyL3JmcaW4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWRzLnNpdGVwb2lu/dC5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMTgvMDMvMTU4/NDQ5NTA3MG1lYW4t/c3RhY2stYW5ndWxh/ci5wbmc"
+},
+  {
+    "id": "2",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "description": "Cras a consectetur tortor, ut auctor est. Vestibulum blandit odio sed leo tempor egestas. Suspendisse tortor odio, maximus ac ante vel, venenatis euismod arcu. Quisque condimentum sed orci vel venenatis.",
+    "photoCover": "https://imgs.search.brave.com/vRCDyL8OvsZZMPcvjiJkFXIlyLNh4ogMVRNKfN-WFKA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/dGVjaGllZGlhcmll/cy5jb20vaW1hZ2Vz/LzIwMjAtMDgtMzEt/cGljdHVyZS1pbi1w/aWN0dXJlLWphdmFz/Y3JpcHQucG5n"
+  }
+];
